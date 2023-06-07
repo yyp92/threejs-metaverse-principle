9e99dfa167fc3d42b7aee691c2eb743fed19013e
yangyipeng001 <892973319@qq.com>
2023-06-07 22:31:24 +0800

    'init'

---
+++ README.md
@@ -0,0 +1,23 @@
+# 元宇宙原理
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## 物理碰撞
+- 采用threejs中的八叉树原理
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+// import App from './App.vue'
+// import App from './App01-1人物控制与碰撞检测.vue'
+// import App from './App01-2控制相机上下跟随人物.vue'
+// import App from './App01-3控制人物动作.vue'
+// import App from './App02-实例化物体提升性能.vue'
+// import App from './App03-合并物体提升性能.vue'
+// import App from './App04-物理材质与光效.vue'
+// import App from './App05-动态世界环境光.vue'
+// import App from './App06-镜面反射.vue'
+// import App from './App07-canvas绘制3d聊天框.vue'
+// import App from './App08-设置alpha填充图片.vue'
+// import App from './App09-视频纹理.vue'
+// 
+import App from './App11-音频位置.vue'
+
+
+
+createApp(App).mount('#app')
